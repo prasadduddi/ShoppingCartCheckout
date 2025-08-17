426276aa5af9cd4a7bb518ae6544758250489c30
Prasad Duddi <prasad.duddi@zoopla.co.uk>
2025-08-17 16:55:31 +0100

    Step 1.4: Updated the prices list to a map

--- src/main/java/com/example/checkout/impl/SimpleCheckout.java
+++ src/main/java/com/example/checkout/impl/SimpleCheckout.java
@@ -4,17 +4,20 @@ import com.example.checkout.core.Checkout;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public class SimpleCheckout implements Checkout {
+
+    private static final Map<String, BigDecimal> PRICE_LIST = Map.of(
+            "apple", new BigDecimal("0.60"),
+            "orange", new BigDecimal("0.25")
+    );
+
     @Override
     public BigDecimal calculateTotal(List<String> items) {
         return items.stream()
                 .map(String::toLowerCase)
-                .map(item -> switch (item) {
-                    case "apple" -> new BigDecimal("0.60");
-                    case "orange" -> new BigDecimal("0.25");
-                    default -> BigDecimal.ZERO;
-                })
+                .map(item -> PRICE_LIST.getOrDefault(item, BigDecimal.ZERO))
                 .reduce(new BigDecimal("0.00"), BigDecimal::add);
     }
 }
